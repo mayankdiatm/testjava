fe420437894293cdbf65244b88b3d710828914d8
Mayank Srivastava <mayankdiatm@gmail.com>
2018-09-01 10:01:56 +0000

    Initial test Commit

---
+++ A.java
@@ -0,0 +1,29 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.io.*;
+
+
+public class A{
+
+
+ public static void main(String[] args)throws IOException{
+ 	
+	String os = System.getProperty("os.name").toLowerCase();
+	System.out.println("The OS Name is " + os);
+        InetAddress ip;
+        String hostname;
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+            System.out.println("Your current IP address : " + ip);
+            System.out.println("Your current Hostname : " + hostname);
+ 
+        } catch (UnknownHostException e) {
+ 
+            e.printStackTrace();
+        }
+    }
+
+}	
+
+
