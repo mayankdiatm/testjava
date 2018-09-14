84434d3dbf915aa89bacc6c169d1684130bf1328
Mayank Srivastava <mayankdiatm@gmail.com>
2018-09-14 05:59:28 +0000

    New Changes Today

--- A.java
+++ A.java
@@ -8,7 +8,7 @@ public class A{
 
  public static void main(String[] args)throws IOException{
  	
-	System.out.println("Hello from Today");
+	System.out.println("Hello from GS");
 	String os = System.getProperty("os.name").toLowerCase();
 	System.out.println("The OS Name is " + os);
         InetAddress ip;
